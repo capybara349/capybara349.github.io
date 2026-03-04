93a067fcf7026577096852b9fd38b47a15061f4b
capybara <1375283285@qq.com>
2026-03-05 04:22:14 +0800

    last update sidebar.

--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -1,90 +1,85 @@
-import { hopeTheme } from "vuepress-theme-hope";
+import {hopeTheme} from "vuepress-theme-hope";
 
 import navbar from "./navbar.js";
 import sidebar from "./sidebar/index.js";
 
 export default hopeTheme({
-  hostname: "https://capybara349.github.io",
+    hostname: "https://capybara349.github.io/",
 
-  logo: "https://theme-hope-assets.vuejs.press/logo.svg",
+    logo: "https://theme-hope-assets.vuejs.press/logo.svg",
 
-  author: {
-    name: "capybara349",
-    url: "https://capybara349.github.io/",
-  },
+    author: {
+        name: "capybara349",
+        url: "https://capybara349.github.io/",
+    },
 
-  repo: "https://github.com/capybara349/capybara349.github.io",
+    repo: "https://github.com/capybara349/capybara349.github.io",
+    docsDir: "docs",
+    pure: true,
+    focus: false,
+    breadcrumb: false,
+    // 导航栏
+    navbar,
+    // 侧边栏
+    sidebar,
+    // 页脚
+    footer: "默认页脚",
+    displayFooter: true,
 
-  docsDir: "docs",
+    // 此处开启了很多功能用于演示，你应仅保留用到的功能。
+    markdown: {
+        align: true,
+        attrs: true,
+        codeTabs: true,
+        component: true,
+        demo: true,
+        figure: true,
+        gfm: true,
+        imgLazyload: true,
+        imgSize: true,
+        include: true,
+        mark: true,
+        plantuml: true,
+        spoiler: true,
+        stylize: [
+            {
+                matcher: "Recommended",
+                replacer: ({tag}) => {
+                    if (tag === "em") {
+                        return {
+                            tag: "Badge",
+                            attrs: {type: "tip"},
+                            content: "Recommended",
+                        };
+                    }
+                },
+            },
+        ],
+        sub: true,
+        sup: true,
+        tabs: true,
+        tasklist: true,
+        vPre: true,
+    },
 
-  // 导航栏
-  navbar,
-
-  // 侧边栏
-  sidebar,
-
-  // 页脚
-  footer: "默认页脚",
-  displayFooter: true,
-
-
-  // 如果想要实时查看任何改变，启用它。注: 这对更新性能有很大负面影响
-  // hotReload: true,
-
-  // 此处开启了很多功能用于演示，你应仅保留用到的功能。
-  markdown: {
-    align: true,
-    attrs: true,
-    codeTabs: true,
-    component: true,
-    demo: true,
-    figure: true,
-    gfm: true,
-    imgLazyload: true,
-    imgSize: true,
-    include: true,
-    mark: true,
-    plantuml: true,
-    spoiler: true,
-    stylize: [
-      {
-        matcher: "Recommended",
-        replacer: ({ tag }) => {
-          if (tag === "em") {
-            return {
-              tag: "Badge",
-              attrs: { type: "tip" },
-              content: "Recommended",
-            };
-          }
+    // 在这里配置主题提供的插件
+    plugins: {
+        // 注意: 仅用于测试! 你必须自行生成并在生产环境中使用自己的评论服务
+        comment: {
+            provider: "Giscus",
+            repo: "vuepress-theme-hope/giscus-discussions",
+            repoId: "R_kgDOG_Pt2A",
+            category: "Announcements",
+            categoryId: "DIC_kwDOG_Pt2M4COD69",
         },
-      },
-    ],
-    sub: true,
-    sup: true,
-    tabs: true,
-    tasklist: true,
-    vPre: true,
-  },
 
-  // 在这里配置主题提供的插件
-  plugins: {
-    // 注意: 仅用于测试! 你必须自行生成并在生产环境中使用自己的评论服务
-    comment: {
-      provider: "Giscus",
-      repo: "vuepress-theme-hope/giscus-discussions",
-      repoId: "R_kgDOG_Pt2A",
-      category: "Announcements",
-      categoryId: "DIC_kwDOG_Pt2M4COD69",
+        components: {
+            components: ["Badge", "VPCard"],
+        },
+
+        icon: {
+            prefix: "fa6-solid:",
+        },
+
     },
-
-    components: {
-      components: ["Badge", "VPCard"],
-    },
-
-    icon: {
-      prefix: "fa6-solid:",
-    },
-
-  },
 });
